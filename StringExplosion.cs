b792ff03bcbeb4dd79caa844467dd1d669d37783
Svetlana Vassileva <72747194+svetlilaloli@users.noreply.github.com>
2021-01-28 14:23:38 +0000

    Add files via upload

---
+++ StringExplosion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StringExplosion
+{
+    class StringExplosion
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int length = text.Length;
+            char punch = '>';
+            string textLeft = "";
+            bool punched = false;
+            int strength = 0;            
+            
+            for (int i = 0; i < length; i++)
+            {
+                if (text[i] == punch)
+                {
+                    punched = true;
+                    textLeft += text[i];
+                    strength += int.Parse(text[i + 1].ToString());
+                }
+                else if (!punched && strength >= 0)
+                {
+                    textLeft += text[i];
+                }
+                else
+                {
+                    strength--;
+                }
+                if (strength == 0)
+                {
+                    punched = false;
+                }
+            }
+            Console.WriteLine(textLeft);
+        }
+    }
+}
